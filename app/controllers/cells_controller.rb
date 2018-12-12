83002c9f650b0c864599a875b4e811a4098aec36
Daniel Burbach and Robert Lee <daniel@>
2018-12-12 15:25:41 -0800

    Add index view.

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -1,5 +1,46 @@
 class CellsController < ApplicationController
+  def index
+    @cells = Cell.all
+  end
 
+  def show
+    @cell = Cell.find(params[:id])
+  end
 
+  def new
+    @cell = Cell.new
+  end
 
+  def create
+    @cell = Cell.new(cell_params)
+    if @cell.save
+      flash[:notice] = "Cell successfully added!"
+      redirect_to cells_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cell = Cell.find(params[:id])
+  end
+
+  def update
+    @cell = Cell.find(params[:id])
+    if @cell.update(cell_params)
+      redirect_to cells_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cell = Cell.find(params[:id])
+    @cell.destroy
+    redirect_to cells_path
+  end
+private
+  def cell_params
+    params.require(:cell).permit(:x,:y,:ship)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root :to => 'cells#index'
+
   resources :cells do
     resources :moves
   end
