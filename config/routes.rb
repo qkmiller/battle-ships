fca5d141e379748bc391bc48e5fdc3aea40df181
Student <example@example.com>
2018-12-18 13:35:32 -0800

    chat 2 starting to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+  root 'chats#show'
+  #temporary root path for now
+
+  resources :users, only:[:new, :create] do
+   resources :chats, only: [:index, :show, :create]
+  end
+  resources :sessions, only:[:new, :destroy, :create]
+  resources :messages, only:[:create]
+
   resources :games do
     resources :cells
   end
