6f4b63990ce5f92f96c3d52cea71912fc0dac1b2
Student <example@example.com>
2018-12-18 10:15:42 -0800

    final 1 initial commit

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -7,7 +7,7 @@ class CellsController < ApplicationController
     # @game.create_game
     @game = Game.find(params[:game_id])
     @game.create_game(params[:game].fetch(:player_1_name))
-    binding.pry
+    
     # @game.create_game(params[:player_1_name],params[:player_2_name])
     @cells = Cell.all.order(:player,:x,:y)
     @moves = Move.all
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,6 @@ class Game < ActiveRecord::Base
   has_many :cells
   def create_game(p1_name)
     self.update({:player_1_name => p1_name})
-    binding.pry
+  
   end
 end
