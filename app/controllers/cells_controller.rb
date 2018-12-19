f166f382c0c18a2cbfb77bbab992642e94323d6c
Student <example@example.com>
2018-12-19 14:24:10 -0800

    add extra win check for player 1

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -22,20 +22,24 @@ class CellsController < ApplicationController
     @cell = Cell.find(params[:id])
     @game = Game.find(@cell.game_id)
     Move.make_move(@cell.x,@cell.y)
-    @moves = Move.all
-    @player_id = Move.who_turn
-    @comp_name = @game.player_2_name
-    @enemy_cell = Cell.all.where(player:1, hit:nil ).order("RANDOM()").first
-
-    Move.make_move(@enemy_cell.x,@enemy_cell.y)
-    @player_id = Move.who_turn
     if Cell.win_check == "p1 wins"
       render :p1_wins
     elsif Cell.win_check == "p2 wins"
       render :p2_wins
     else
-      render :index
+      @moves = Move.all
+      @player_id = Move.who_turn
+      @comp_name = @game.player_2_name
+      @enemy_cell = Cell.all.where(player:1, hit:nil ).order("RANDOM()").first
+      Move.make_move(@enemy_cell.x,@enemy_cell.y)
+      @player_id = Move.who_turn
+      if Cell.win_check == "p1 wins"
+        render :p1_wins
+      elsif Cell.win_check == "p2 wins"
+        render :p2_wins
+      else
+        render :index
+      end
     end
-
   end
 end
